5cd255a02dd3dfcde9bc13c53258bc87f9f6842e
Xia Liu <xialiu1998@gmail.com>
2020-02-25 09:47:29 -0600

    Fix flake8 issues

--- gnali/variants.py
+++ gnali/variants.py
@@ -12,14 +12,15 @@ class Variant:
 
     def __init__(self, record):
         self.chrom, self.pos, self.id, self.ref, \
-        self.alt, self.qual, self.filter, self.info_str = record.split("\t")
+                    self.alt, self.qual, self.filter, \
+                    self.info_str = record.split("\t")
         self.info = dict([info_item.split("=") for
                           info_item in self.info_str.split(";")
                           if len(info_item.split("=")) > 1])
 
     def __str__(self):
         return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n" \
-               .format(self.chrom, self.pos, self.id, self.ref, \
+               .format(self.chrom, self.pos, self.id, self.ref,
                        self.alt, self.qual, self.filter, self.info_str)
 
     @classmethod
@@ -29,5 +30,5 @@ class Variant:
                 "Score", "Quality", "Codes"]
 
     def as_tuple(self):
-        return (self.chrom, self.pos, self.id, self.ref, \
+        return (self.chrom, self.pos, self.id, self.ref,
                 self.alt, self.qual, self.filter, self.info_str)
