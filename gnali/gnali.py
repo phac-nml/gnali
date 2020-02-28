ea5834790d4859482f9caf9c54a6d9e6905c81b0
Xia Liu <xialiu1998@gmail.com>
2020-02-28 09:17:48 -0600

    Fix file locking

--- gnali/gnali.py
+++ gnali/gnali.py
@@ -171,16 +171,14 @@ def get_db_tbi(database, data_path, max_time):
     lock = FileLock(tbi_lock)
 
     try:
-        lock.acquire(timeout=max_time)
-        download_file(tbi_url, tbi_path, max_time)
+        with lock.acquire(timeout=max_time):
+            download_file(tbi_url, tbi_path, max_time)
     # not able to gain access to index in time
     except TimeoutError:
         # download index file to temp directory
         temp = tempfile.TemporaryDirectory()
         tbi_path = "{}/{}".format(temp.name, tbi_name)
         download_file(tbi_url, tbi_path, max_time)
-    finally:
-        lock.release()
 
     return tbi_path
 
