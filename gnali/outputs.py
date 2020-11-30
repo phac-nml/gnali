a0e598c1115a7f66d3565f7a58a702c231e7a123
Xia Liu <xialiu1998@gmail.com>
2020-11-30 13:35:26 -0600

    Fix write_to_vcf

--- gnali/outputs.py
+++ gnali/outputs.py
@@ -24,10 +24,12 @@ def write_to_tab(path, data):
 def write_to_vcf(path, headers, data):
     with open(path, 'w+') as stream:
         for line in headers:
-            stream.write(str(line))
+            line = str(line)
+            stream.write(line)
             if line[-1] != '\n':
                 stream.write('\n')
         for line in data:
-            stream.write(str(line))
+            line = str(line)
+            stream.write(line)
             if line[-1] != '\n':
                 stream.write('\n')
