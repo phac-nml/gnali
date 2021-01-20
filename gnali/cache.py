78b5d782c57b9c06edd084e7ec261cf15fcd0564
Xia Liu <xialiu1998@gmail.com>
2021-01-20 17:03:23 -0600

    Remove unnecessary import

--- gnali/cache.py
+++ gnali/cache.py
@@ -16,7 +16,6 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
-from pathlib import Path
 import os
 import subprocess
 from gnali.exceptions import ReferenceDownloadError
