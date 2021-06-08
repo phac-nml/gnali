2aed564dd73a4bbbf4acfd3a58e0433e57f69e68
Xia Liu <xialiu1998@gmail.com>
2021-06-08 15:20:30 -0500

    Move filtering to inside of variant-fetching loop

--- gnali/gnali.py
+++ gnali/gnali.py
@@ -341,7 +341,7 @@ def get_variants(genes, db_info, filter_objs, output_dir,
         logger: Logger object to log errors to
         verbose_on: boolean for verbose mode
     """
-    variants = []
+    variants = np.array([])
     max_time = 180
     header = None
     temp_dir = tempfile.TemporaryDirectory()
@@ -374,7 +374,24 @@ def get_variants(genes, db_info, filter_objs, output_dir,
 
                 # update to convert to Variants before filter calls
                 records = [Variant(gene.name, record) for record in records]
-                variants.extend(records)
+
+                if not db_info.has_lof_annots:
+                    header, variants = VEP.annotate_vep_loftee(header,
+                                                               variants,
+                                                               db_info)
+                lof_index = None
+                # get index of LoF in header
+                annot_header = [line for line in header
+                                if "ID={}".format(db_info.lof['id'])
+                                in line]
+                lof_index = str(annot_header).split("|") \
+                    .index(db_info.lof['annot'])
+
+                # filter records
+                records = filter_plof(genes, records, db_info, lof_index)
+                records = apply_filters(genes, records, db_info, filter_objs)
+
+                variants = np.concatenate([variants, records])
             except ValueError as error:
                 # ValueError means that location used in TabixFile.fetch()
                 # does not exist in the database
@@ -384,7 +401,8 @@ def get_variants(genes, db_info, filter_objs, output_dir,
                                  "in database {}"
                                  .format(gene.name, error, data_file.name,
                                          db_info.name))
-            except Exception:
+            except Exception as error:
+                print(error)
                 raise
 
     # Set error status for gene if it wasn't found in any database file
@@ -392,20 +410,6 @@ def get_variants(genes, db_info, filter_objs, output_dir,
         if not coverage[gene.name] and gene.status is None:
             gene.set_status("No variants in database")
 
-    if not db_info.has_lof_annots:
-        header, variants = VEP.annotate_vep_loftee(header, variants,
-                                                   db_info)
-
-    lof_index = None
-    # get index of LoF in header
-    annot_header = [line for line in header
-                    if "ID={}".format(db_info.lof['id'])
-                    in line]
-    lof_index = str(annot_header).split("|") \
-        .index(db_info.lof['annot'])
-
-    variants = filter_plof(genes, variants, db_info, lof_index)
-    variants = apply_filters(genes, variants, db_info, filter_objs)
     return header, variants
 
 
